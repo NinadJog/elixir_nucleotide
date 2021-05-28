b21a03f68dd07e1000ecb551d9b4d069a4078bbb
NinadJog <Ninad.Jog@gmail.com>
2021-05-28 15:34:41 -0400

    Initial commit

---
+++ lib/elixir_nucleotide.ex
@@ -0,0 +1,19 @@
+defmodule ElixirNucleotide do
+  @moduledoc """
+  Documentation for `ElixirNucleotide`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirNucleotide.hello()
+      :world
+
+  """
+  def hello do
+    IO.puts "Hello world"
+    :world
+  end
+end
---
+++ test/elixir_nucleotide_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirNucleotideTest do
+  use ExUnit.Case
+  doctest ElixirNucleotide
+
+  test "greets the world" do
+    assert ElixirNucleotide.hello() == :world
+  end
+end
